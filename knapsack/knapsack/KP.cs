ad1eef25b06cdb1f229a53ae72166b15dd80f152
Igor Kliushnikov <ma3str0@i.ua>
2013-06-28 14:50:11 +0300

    Changed processor

--- knapsack/knapsack/KP.cs
+++ knapsack/knapsack/KP.cs
@@ -67,11 +67,53 @@ namespace knapsack
 
         public string Solution()
         {
+            int maxValue = 0;
+            int maxIndex = 0;
+            for (int i = 1; i < _elementsCount + 1; i++)
+            {
+                int currentValue = values[_capacity, i];
+                if (maxValue < currentValue)
+                {
+                    maxValue = currentValue;
+                    maxIndex = i - 1;
+                }
+            }
 
+            XElement maxElement = _elements[maxIndex];
+            maxElement.IsIncluded = true;
 
-            IEnumerable<int> enumerable = _elements.Select(i => i.IsIncluded ? 1 : 0);
+            MarkNext(FindLastColumnChange(maxIndex + 1) - maxElement.Weight, maxIndex);
 
-            return string.Join(" ", enumerable);
+            string els = string.Join(" ", _elements.Select(i => i.IsIncluded ? 1 : 0));
+
+            return string.Format("{0} {1}{2}{3}", maxValue, 0, Environment.NewLine, els);
+        }
+
+        private int FindLastColumnChange(int maxIndex)
+        {
+            for (int i = _capacity; i > 1; i--)
+            {
+                if (values[i,maxIndex] != values[i-1, maxIndex])
+                {
+                    return i;
+                }
+            }
+            return 0;
+        }
+
+        private void MarkNext(int currentWeight, int currentIndex)
+        {
+            if (values[currentWeight, currentIndex - 1] == values[currentWeight, currentIndex])
+            {
+                MarkNext(currentWeight, currentIndex - 1);
+            }
+            else
+            {
+                var element = _elements[currentIndex - 1];
+                element.IsIncluded = true;
+                if (currentWeight > element.Weight && currentIndex > 1)
+                    MarkNext(currentWeight - element.Weight, currentIndex - 1);
+            }
         }
 
         public void Dispose()
